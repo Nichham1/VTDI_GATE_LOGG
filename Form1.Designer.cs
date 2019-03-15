029fb766675c7fdaee4c0faf7aa8fe7d6f5cd5e3
Nichham1 <nichham@gmail.com>
2019-03-14 19:38:50 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace VTDI_GATE_LOGG
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.userToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gateLogsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.reportsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.logInToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.userManagementToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.schedulingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gateListToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gateInToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gateReportsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.userLogsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.userToolStripMenuItem,
+            this.gateLogsToolStripMenuItem,
+            this.reportsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(673, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // userToolStripMenuItem
+            // 
+            this.userToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.logInToolStripMenuItem,
+            this.userManagementToolStripMenuItem,
+            this.schedulingToolStripMenuItem});
+            this.userToolStripMenuItem.Name = "userToolStripMenuItem";
+            this.userToolStripMenuItem.Size = new System.Drawing.Size(42, 20);
+            this.userToolStripMenuItem.Text = "User";
+            // 
+            // gateLogsToolStripMenuItem
+            // 
+            this.gateLogsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gateListToolStripMenuItem,
+            this.gateInToolStripMenuItem});
+            this.gateLogsToolStripMenuItem.Name = "gateLogsToolStripMenuItem";
+            this.gateLogsToolStripMenuItem.Size = new System.Drawing.Size(68, 20);
+            this.gateLogsToolStripMenuItem.Text = "GateLogs";
+            // 
+            // reportsToolStripMenuItem
+            // 
+            this.reportsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gateReportsToolStripMenuItem,
+            this.userLogsToolStripMenuItem});
+            this.reportsToolStripMenuItem.Name = "reportsToolStripMenuItem";
+            this.reportsToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.reportsToolStripMenuItem.Text = "Reports";
+            // 
+            // logInToolStripMenuItem
+            // 
+            this.logInToolStripMenuItem.Name = "logInToolStripMenuItem";
+            this.logInToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.logInToolStripMenuItem.Text = "Log in";
+            // 
+            // userManagementToolStripMenuItem
+            // 
+            this.userManagementToolStripMenuItem.Name = "userManagementToolStripMenuItem";
+            this.userManagementToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.userManagementToolStripMenuItem.Text = "User Management";
+            // 
+            // schedulingToolStripMenuItem
+            // 
+            this.schedulingToolStripMenuItem.Name = "schedulingToolStripMenuItem";
+            this.schedulingToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.schedulingToolStripMenuItem.Text = "Scheduling";
+            // 
+            // gateListToolStripMenuItem
+            // 
+            this.gateListToolStripMenuItem.Name = "gateListToolStripMenuItem";
+            this.gateListToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.gateListToolStripMenuItem.Text = "Gate List";
+            // 
+            // gateInToolStripMenuItem
+            // 
+            this.gateInToolStripMenuItem.Name = "gateInToolStripMenuItem";
+            this.gateInToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.gateInToolStripMenuItem.Text = "Gate In";
+            // 
+            // gateReportsToolStripMenuItem
+            // 
+            this.gateReportsToolStripMenuItem.Name = "gateReportsToolStripMenuItem";
+            this.gateReportsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.gateReportsToolStripMenuItem.Text = "Gate Reports";
+            // 
+            // userLogsToolStripMenuItem
+            // 
+            this.userLogsToolStripMenuItem.Name = "userLogsToolStripMenuItem";
+            this.userLogsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.userLogsToolStripMenuItem.Text = "User Logs";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(673, 630);
+            this.Controls.Add(this.menuStrip1);
+            this.HelpButton = true;
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem userToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem logInToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem userManagementToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem schedulingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gateLogsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gateListToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gateInToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem reportsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gateReportsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem userLogsToolStripMenuItem;
+    }
+}
+
